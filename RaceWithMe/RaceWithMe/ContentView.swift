dc9e78470f8219a01c4ebc3b9c9065addb081ddc
Robert Theo <robertheo.rt@gmail.com>
2024-04-29 17:41:26 +0700

    initial project

---
+++ RaceWithMe/RaceWithMe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RaceWithMe
+//
+//  Created by robert theo on 29/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RaceWithMe/RaceWithMe/RaceWithMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaceWithMeApp.swift
+//  RaceWithMe
+//
+//  Created by robert theo on 29/04/24.
+//
+
+import SwiftUI
+
+@main
+struct RaceWithMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
